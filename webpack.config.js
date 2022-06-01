abdd90c9572bfebfc0061bd094449759313f1388
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-06-01 16:25:05 +0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   optimization: {
