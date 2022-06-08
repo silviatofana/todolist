23b480d9f2c13ae650becb51d4cc3df6506bff4b
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-06-08 12:26:21 +0300

    changes

--- src/modules/addTask.js
+++ src/modules/addTask.js
@@ -1,13 +1,8 @@
-const listBody = document.getElementById('list-body');
-const checkbox = document.querySelector('input[name=check]');
-const taskDescription = document.querySelector('.taskdescription');
-const addList = document.querySelector('input[name=addtext]');
-const clear = document.getElementById('clear');
-
-export {
-  listBody,
-  checkbox,
-  taskDescription,
-  addList,
-  clear,
+const addTask = (tasklist, newTask) => {
+  newTask.id = tasklist.length + 1 || 1;
+  newTask.completed = false;
+  tasklist.push(newTask);
+  return tasklist;
 };
+
+export default addTask;
--- src/modules/createLi.js
+++ src/modules/createLi.js
@@ -1,73 +1,20 @@
-export default class ToDoList {
-  constructor() {
-    this.getTasks();
+const createLi = (newTask) => {
+  const li = document.createElement('li');
+  const checkbox = document.createElement('input');
+  const description = document.createElement('input');
+  const i = document.createElement('i');
+  description.type = 'text';
+  description.value = `${newTask.description}`;
+  description.desabled = true;
+  checkbox.type = 'checkbox';
+  i.classList.add('fa-solid', 'fa-ellipsis-vertical');
+  li.classList.add(newTask.id);
+  li.append(checkbox, description, i);
+  document.querySelector('.taskList').prepend(li);
+  if (newTask.completed) {
+    checkbox.checked = true;
+    li.classList.add('checked');
   }
+};
 
-  updateTasks() {
-    localStorage.setItem('toDoList', JSON.stringify(this.listArray));
-  }
-
-  getTasks() {
-    this.listArray = JSON.parse(localStorage.getItem('toDoList')) || [];
-  }
-
-    showTasks = () => JSON.parse(localStorage.getItem('toDoList')) || [];
-
-    setEdit(i) {
-      const task = this.listArray.find(
-        (item) => parseInt(item.index, 10) === parseInt(i, 10),
-      );
-      task.edit = true;
-      this.updateTasks();
-    }
-
-    // add a task
-    addTask(description) {
-      const task = {
-        description,
-        completed: false,
-        index: this.listArray.length + 1,
-        edit: false,
-      };
-      this.listArray = [...this.listArray, task];
-      this.updateTasks();
-    }
-
-    // remove a task
-    clearCompleted() {
-      this.listArray = this.listArray.filter((item) => item.completed !== true);
-      if (this.listArray.length > 0) {
-        this.listArray = this.listArray.map((list, i) => {
-          list.index = i + 1;
-          return list;
-        });
-      }
-      this.updateTasks();
-    }
-
-    // remove a task
-    removeTask(index) {
-      this.listArray = this.listArray.filter((item) => item.index !== index);
-      this.listArray = this.listArray.map((list, i) => {
-        list.index = i + 1;
-        return list;
-      });
-      this.updateTasks();
-    }
-
-    // Edit a task
-    editTask(index, description) {
-      this.listArray[index - 1].description = description;
-      this.listArray[index - 1].edit = false;
-      this.updateTasks();
-    }
-
-    changeComplete(i) {
-      const status = this.listArray[i - 1].completed;
-      this.listArray[i - 1] = {
-        ...this.listArray[i - 1],
-        completed: !status,
-      };
-      this.updateTasks();
-    }
-}
+export default createLi;
